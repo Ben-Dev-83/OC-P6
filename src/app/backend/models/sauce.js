9d44231625286ff0c932112d5bce567b1297b798
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-01-21 00:39:06 +0100

    Création du models sauce

---
+++ src/app/backend/models/sauce.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const sauceSchema = mongoose.Schema({ 
+    name: { type: String, require: true },
+    userId: {type: String, require: true},
+    manufacturer: {type: String, require: true},
+    description: {type: String, require: true},
+    mainPepper: {type: String, require: true},
+    imageUrl: {type: String, require: true},
+    heat: {type: Number},
+    likes: {type: Number},
+    dislikes: {type: Number},
+    usersLiked: {type: Array },
+    usersDisliked: {type: Array },
+});
+
+
+module.exports = mongoose.model('Sauce', sauceSchema)
