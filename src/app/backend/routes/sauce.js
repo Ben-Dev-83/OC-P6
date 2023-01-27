de7d4a4249f0017c1aeb2df83ec3205a501d98e2
Benjamin Bonvicini <benjamin.bonvicini83@gmail.com>
2023-01-27 16:42:52 +0100

    Création des routes sauce

--- src/app/backend/routes/sauce.js
+++ src/app/backend/routes/sauce.js
@@ -4,7 +4,11 @@ const auth = require('../middleware/auth')
 const multer = require('../middleware/multer-config')
 const sauceCtrl = require('../controllers/sauce')
 
-router.get('/sauces', auth, sauceCtrl.getAllSauce)
-router.post('/sauces', auth, multer, sauceCtrl.newSauce)
+router.get('/', auth, sauceCtrl.getAllSauce)
+router.post('/', auth, multer, sauceCtrl.createSauce)
+router.post('/:id/like', auth, sauceCtrl.likeSauce)
+router.get('/:id', auth, sauceCtrl.getOneSauce)
+router.put('/:id', auth, sauceCtrl.modifySauce)
+router.delete('/:id', auth, sauceCtrl.deleteSauce)
 
   module.exports = router
